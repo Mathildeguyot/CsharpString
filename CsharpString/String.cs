c28fc33be67226810d42ea25fad619483f798708
Mathilde Guyot <mathilde.guyot1@gmail.com>
2022-04-28 15:53:10 +0200

    Implement string class and tests

---
+++ CsharpString/String.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace CsharpString
+{
+    public class String : IEquatable<String>
+    {
+        public String(char[] value)
+        {
+            charArray = value ?? throw new ArgumentNullException(nameof(value), "Parameter cannot be null");
+        }
+
+        public String(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value), "Parameter cannot be null");
+            charArray = value.ToCharArray();
+        }
+
+        public static String Concat(String s1, String s2)
+        {
+            char[] concatenated = new char[s1.Length + s2.Length];
+
+            s1.charArray.CopyTo(concatenated, 0);
+            s2.charArray.CopyTo(concatenated, s1.Length);
+
+            return new String(concatenated);
+        }
+
+        public bool Equals(String other)
+        {
+            if (Length == other.Length)
+            {
+                if (Length == 0 && other.Length == 0)
+                {
+                    return true;
+                }
+                for (int i = 0; i < Length; i++)
+                {
+                    if (charArray[i] != other.charArray[i])
+                    {
+                        return false;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || obj.GetType() != typeof(string))
+                return false;
+
+            return Equals(new String(obj as string));
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public int IndexOf(char value)
+        {
+            return Array.IndexOf(charArray, value);
+        }
+
+        public String Insert(int index, String s1)
+        {
+            if (index < 0 || index > Length)
+                throw new ArgumentOutOfRangeException("index", index, "Argument is out of range");
+
+            char[] inserted = new char[s1.Length + charArray.Length];
+
+            Array.Copy(charArray, 0, inserted, 0, index);
+            Array.Copy(s1.charArray, 0, inserted, index, s1.Length);
+            Array.Copy(charArray, index, inserted, index + s1.Length, Length - index);
+
+            return new String(inserted);
+        }
+
+        public override string ToString()
+        {
+            return new string(charArray);
+        }
+
+        public char this[int index] => charArray[index];
+        public int Length => charArray.Length;
+
+        private readonly char[] charArray;
+    }
+}
---
+++ CsharpStringTests/UnitTest1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Xunit;
+
+namespace CsharpStringTests;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("toto", "tata", "tototata")]
+    [InlineData("", "tata", "tata")]
+    [InlineData("toto", "", "toto")]
+    public void Concat_ShouldReturnTwoStringsConcatenated(string s1, string s2, string s3)
+    {
+        CsharpString.String value1 = new(s1);
+        CsharpString.String value2 = new(s2);
+        CsharpString.String expected = new(s3);
+
+        CsharpString.String actual = CsharpString.String.Concat(value1, value2);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("tut", null, false)]
+    [InlineData("tata", "tat", false)]
+    [InlineData("tata", 1, false)]
+    public void Equals_ShouldReturnFalseIfTwoSObjectsHaveNotTheSameContent(string s1, object o1, bool expected)
+    {
+        CsharpString.String first = new(s1);
+
+        bool actual = first.Equals(o1);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("tut", "", false)]
+    [InlineData("tata", "taa", false)]
+    public void Equals_ShouldReturnFalseIfTwoStringsHaveNotTheSameContent(string s1, string s2, bool expected)
+    {
+        CsharpString.String first = new(s1);
+        CsharpString.String second = new(s2);
+
+        bool actual = first.Equals(second);
+
+        Assert.Equal(expected, actual);
+    }
+
+
+    [Theory]
+    [InlineData("", "", true)]
+    [InlineData("tata", "tata", true)]
+    public void Equals_ShouldReturnTrueIfTwoObjectsHaveTheSameContent(string s1, object o1, bool expected)
+    {
+        CsharpString.String first = new(s1);
+
+        bool actual = first.Equals(o1);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("", "", true)]
+    [InlineData("tata", "tata", true)]
+    public void Equals_ShouldReturnTrueIfTwoStringsHaveTheSameContent(string s1, string s2, bool expected)
+    {
+        CsharpString.String first = new(s1);
+        CsharpString.String second = new(s2);
+
+        bool actual = first.Equals(second);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("tata", "tat")]
+    [InlineData("tata", "toto")]
+    public void GetHashCode_ShouldNotReturnTheSameValueForTwoStringsWithDifferentContent(string s1, string s2)
+    {
+        CsharpString.String first = new(s1);
+        CsharpString.String second = new(s2);
+
+        int firstHashCode = first.GetHashCode();
+        int secondHashCode = second.GetHashCode();
+
+        Assert.NotEqual(firstHashCode, secondHashCode);
+    }
+
+    [Theory]
+    [InlineData("tata", "tata")]
+    [InlineData("", "")]
+    public void GetHashCode_ShouldReturnTheSameValueForTwoStringsWithTheSameContent(string s1, string s2)
+    {
+        CsharpString.String first = new(s1);
+        CsharpString.String second = new(s2);
+
+        int firstHashCode = first.GetHashCode();
+        int secondHashCode = second.GetHashCode();
+
+        Assert.Equal(firstHashCode, secondHashCode);
+    }
+
+
+    [Theory]
+    [InlineData("tato", 3, 'o')]
+    [InlineData("tato", 0, 't')]
+    public void GetIndex_ShouldReturnTheCharacterOfTheGivenIndex(string s1, int index, char expected)
+    {
+        CsharpString.String value = new(s1);
+
+        char actual = value[index];
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("toto", 'o', 1)]
+    [InlineData("toto", 'a', -1)]
+    public void IndexOf_ShouldReturnTheIndexOfTheGivenCharacter(string s1, char c1, int expected)
+    {
+        CsharpString.String value = new(s1);
+
+        int actual = value.IndexOf(c1);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("toto", " ", 2, "to to")]
+    [InlineData("tata", "tutu", 0, "tututata")]
+    public void Insert_ShouldReturnANewStringWithInsertedValueAtCorrectIndex(string s1, string s2, int index, string s3)
+    {
+        CsharpString.String first = new(s1);
+        CsharpString.String second = new(s2);
+        CsharpString.String expected = new(s3);
+
+        CsharpString.String actual = first.Insert(index, second);
+
+        Assert.Equal(actual, expected);
+    }
+
+    [Theory]
+    [InlineData("tata", "tutu", -10)]
+    [InlineData("tata", "tutu", 10)]
+    public void Insert_SouldThrowExceptionWhenIndexIsOutOfRange(string s1, string s2, int index)
+    {
+        CsharpString.String first = new(s1);
+        CsharpString.String second = new(s2);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => first.Insert(index, second));
+    }
+
+    [Theory]
+    [InlineData("toto", 4)]
+    [InlineData("", 0)]
+    public void Length_ShouldReturnTheNumberOfCharactersInTheString(string s1, int expectedLength)
+    {
+        CsharpString.String value = new(s1);
+
+        int actual = value.Length;
+
+        Assert.Equal(expectedLength, actual);
+    }
+
+    [Fact]
+    public void String_ConstructorWithNullArgumentShouldThrowArgumentException()
+    {
+        string nullString = null;
+
+        Assert.Throws<ArgumentNullException>(() => new CsharpString.String(nullString));
+    }
+
+    [Theory]
+    [InlineData("toto")]
+    [InlineData("")]
+    public void ToString_ShouldReturnASystemString(string s1)
+    {
+        CsharpString.String value = new(s1);
+
+        var actual = value.ToString();
+
+        Assert.IsType<string>(actual);
+    }
+}
